acba7d49f5e25077ff5959d946509890609879f6
Andressa Karoline <andressakdogado@gmail.com>
2019-09-12 09:26:25 -0300

    Update - Atualizando o contato

--- src/pages/contacts-list/contacts-list.ts
+++ src/pages/contacts-list/contacts-list.ts
@@ -53,6 +53,18 @@ export class ContactsListPage {
     });
   }
 
+  openEditContact(id: number) {
+    this.contactsProvider.getContact(id)
+    .then((result: any) => {
+      this.navCtrl.push('ContactEditPage',  { 
+        contact: result 
+      });
+    })
+    .catch((error: any) => {
+      this.toast.create({ message: error.error }).present();
+    });
+  }
+  
   doRefresh(refresher) {
     setTimeout(() => {
       this.getContacts();
